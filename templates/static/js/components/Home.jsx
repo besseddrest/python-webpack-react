1a24c61f0aa113a8aa1497c48dcf8944f5633193
Harold Cabalic <hcabalic@Harolds-MacBook-Pro.local>
2019-12-11 12:43:58 -0800

    some styles

--- templates/static/js/components/Home.jsx
+++ templates/static/js/components/Home.jsx
@@ -3,13 +3,25 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div className="grid-container">
-        <div className="grid-x grid-padding-x">
-          <div className="cell">
-            <h1>It Really Does Go</h1>
-            <a className="button" href="/bass">Basses</a>
+      <div>
+        <header className="header">
+          <div className="grid-container">
+            <div className="grid-x grid-padding-x">
+              <div className="cell">
+                <h1>It Really Does Go</h1>
+              </div>
+            </div>
           </div>
-        </div>
+        </header>
+        <main className="main">
+          <div className="grid-container">
+            <div className="grid-x grid-padding-x">
+              <div className="cell">
+                <a className="button" href="/bass">Basses</a>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     )
   }
