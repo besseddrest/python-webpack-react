806f389b44fca578576398133758e1a51ea4b2d4
besseddrest <hcabalic@gmail.com>
2019-12-11 10:18:23 -0800

    update packages

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -21,6 +21,10 @@ const config = {
         query: {
           presets: ['react','es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules'
       }
     ]
   }
