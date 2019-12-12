29101f6952875b99394691aa0ef887a92fc189c3
Harold Cabalic <hcabalic@Harolds-MacBook-Pro.local>
2019-12-11 16:01:01 -0800

    make call to /basses endpoint display data

--- templates/static/js/components/Bass.jsx
+++ templates/static/js/components/Bass.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Bass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/basses')
+      .then(res => res.json())
+        .then((result) => {
+          this.setState({
+            items: result
+          });
+        });
+  }
+
   render() {
+    const items = this.state.items;
+
     return (
       <div>
         <header className="header">
@@ -13,6 +32,20 @@ export default class Bass extends Component {
             </div>
           </div>
         </header>
+
+        <main className="main">
+          <div className="grid-container">
+            <div className="grid-x grid-padding-x">
+              <ul>
+                {
+                  items.map((item, i) => (
+                    <li key={i}><strong>{item.make}:</strong> {item.model}</li>
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+        </main>
       </div>
     )
   }
