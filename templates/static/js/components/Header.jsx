af7184c44b35274690dc85c02c7fe30abb6d1a82
Harold Cabalic <hcabalic@Harolds-MacBook-Pro.local>
2019-12-13 11:09:02 -0800

    improve layout

--- templates/static/js/components/Header.jsx
+++ templates/static/js/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
   constructor(props) {
@@ -10,12 +11,21 @@ export default class Header extends Component {
       <header className="header">
         <div className="grid-container">
           <div className="grid-x grid-padding-x">
-            <div className="cell">
-              <h1>{this.props.heading}</h1>
+            <div className="cell auto">
+              <div className="top-bar">
+                <div className="top-bar-left">
+                  <h1><Link to="/">{this.props.heading}</Link></h1>
+                </div>
+                <div className="top-bar-right">
+                  <ul className="menu horizontal">
+                    <li><NavLink to="/bass">Basses</NavLink></li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </header>
     )
   }
-};
+}; 
--- templates/static/js/components/Home.jsx
+++ templates/static/js/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   constructor(props) {
@@ -12,7 +11,7 @@ export default class Home extends Component {
         <div className="grid-container">
           <div className="grid-x grid-padding-x">
             <div className="cell auto">
-              <Link className="button" to="/bass">Basses</Link>
+              <p>A listing of my music instruments</p>
             </div>
           </div>
         </div>
